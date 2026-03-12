a55fe752d55f5897fa4cd873194b50079a5d9754
Simon Steinrücken <simon.steinruecken@camunda.com>
2026-03-12 17:38:21 +0100

    chore: add ootb templates

---
+++ test/spec/element-templates/CreateAppendCamundaElementTemplates.spec.js
@@ -0,0 +1,127 @@
+import TestContainer from 'mocha-test-container-support';
+
+import { expect } from 'chai';
+
+import {
+  clearBpmnJS,
+  setBpmnJS,
+  insertCoreStyles,
+  insertBpmnStyles,
+  enableLogging
+} from 'test/TestHelper';
+
+import Modeler from 'bpmn-js/lib/Modeler';
+
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+  ZeebePropertiesProviderModule,
+} from 'bpmn-js-properties-panel';
+
+import {
+  CloudElementTemplatesPropertiesProviderModule as ElementTemplatesProviderModule
+} from 'bpmn-js-element-templates';
+
+import { CreateAppendElementTemplatesModule, CreateAppendAnythingModule } from 'lib/';
+
+import ZeebeModdle from 'zeebe-bpmn-moddle/resources/zeebe';
+
+import ElementTemplateChooserModule from '@bpmn-io/element-template-chooser';
+
+import templates from 'test/fixtures/camunda-connector-templates.json';
+
+
+const singleStart = window.__env__ && window.__env__.SINGLE_START === 'camunda-templates';
+
+insertCoreStyles();
+insertBpmnStyles();
+
+
+describe('<CreateAppendAnything> with Camunda connector templates', function() {
+
+  let modelerContainer;
+
+  let propertiesContainer;
+
+  let container;
+
+  beforeEach(function() {
+    modelerContainer = document.createElement('div');
+    modelerContainer.classList.add('modeler-container');
+
+    propertiesContainer = document.createElement('div');
+    propertiesContainer.classList.add('properties-container');
+
+    container = TestContainer.get(this);
+
+    container.appendChild(modelerContainer);
+    container.appendChild(propertiesContainer);
+  });
+
+  async function createModeler(xml, options = {}, BpmnJS = Modeler) {
+    const {
+      shouldImport = true,
+      additionalModules = [
+        BpmnPropertiesPanelModule,
+        BpmnPropertiesProviderModule,
+        ZeebePropertiesProviderModule,
+        ElementTemplatesProviderModule,
+        ElementTemplateChooserModule,
+        CreateAppendAnythingModule,
+        CreateAppendElementTemplatesModule
+      ],
+      moddleExtensions = {
+        zeebe: ZeebeModdle
+      },
+      description = {},
+      layout = {}
+    } = options;
+
+    clearBpmnJS();
+
+    const modeler = new BpmnJS({
+      container: modelerContainer,
+      additionalModules,
+      propertiesPanel: {
+        parent: propertiesContainer,
+        feelTooltipContainer: container,
+        description,
+        layout
+      },
+      moddleExtensions,
+      ...options
+    });
+
+    enableLogging && enableLogging(modeler, !!singleStart);
+
+    setBpmnJS(modeler);
+
+    if (!shouldImport) {
+      return { modeler };
+    }
+
+    try {
+      const result = await modeler.importXML(xml);
+
+      return { error: null, warnings: result.warnings, modeler: modeler };
+    } catch (err) {
+      return { error: err, warnings: err.warnings, modeler: modeler };
+    }
+  }
+
+
+  (singleStart ? it.only : it)('should import simple process with Camunda connector templates', async function() {
+
+    // given
+    const diagramXml = require('test/fixtures/simple.bpmn').default;
+
+    // when
+    const { error, modeler } = await createModeler(diagramXml);
+
+    modeler.get('elementTemplates').set(templates);
+
+    // then
+    expect(error).not.to.exist;
+  });
+
+});
